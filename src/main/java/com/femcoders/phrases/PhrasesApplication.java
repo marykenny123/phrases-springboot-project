debe4d8f5aefd387331546b34ee1285a2a196858
mary kenny <marykenny2704@yahoo.com>
2025-06-10 14:52:13 +0200

    initial commit before linking to github

---
+++ src/main/java/com/femcoders/phrases/PhrasesApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.phrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/phrases/PhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.phrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
